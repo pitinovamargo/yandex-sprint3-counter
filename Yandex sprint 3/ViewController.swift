df6e1579c3022c9a836c0d56db3af5d5bd5965ea
Margarita Pitinova <pitinovamargo@gmail.com>
2023-01-30 22:55:18 +0100

    Created Counter app

--- Yandex sprint 3/ViewController.swift
+++ Yandex sprint 3/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counter: UILabel!
+    private var number: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func addValue(_ sender: Any) {
+        number += 1
+        counter.text = "Значение счетчика: \(number)"
+    }
+    
 }
 
