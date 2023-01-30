1a9ed57f8b5089fb204a2072bad83875042a8289
Margarita Pitinova <pitinovamargo@gmail.com>
2023-01-30 22:00:59 +0100

    Initial Commit

---
+++ Yandex sprint 3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yandex sprint 3
+//
+//  Created by Margarita Pitinova on 30.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
